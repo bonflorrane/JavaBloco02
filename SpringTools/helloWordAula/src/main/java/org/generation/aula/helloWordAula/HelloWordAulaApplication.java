4dc430111bf903f51092146437ddd1da10f46bfb
bonflorrane <bonfim_lorrane@hotmail.com>
2022-03-23 10:39:27 -0300

    atualizando Spring e Der

---
+++ SpringTools/helloWordAula/src/main/java/org/generation/aula/helloWordAula/HelloWordAulaApplication.java
@@ -0,0 +1,13 @@
+package org.generation.aula.helloWordAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordAulaApplication.class, args);
+	}
+
+}
---
+++ SpringTools/helloWordAula/src/test/java/org/generation/aula/helloWordAula/HelloWordAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.aula.helloWordAula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
