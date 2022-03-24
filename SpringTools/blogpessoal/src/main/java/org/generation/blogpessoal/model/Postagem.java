dedad8926353549964b55d0528c6ae0549b3fbec
bonflorrane <bonfim_lorrane@hotmail.com>
2022-03-24 16:33:09 -0300

    atualizando spring

---
+++ SpringTools/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,67 @@
+package org.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity //que tudo que colocar aqui irá virar uma tabela
+@Table (name="tb_postagens") // serve para colocar o nome da tabela, = create table
+
+public class Postagem {
+
+	@Id // indica que o que vir dentro aqui será uma chave primária
+	@GeneratedValue(strategy=GenerationType.IDENTITY) // responsável pelo auto_incrmento
+	private Long id;
+	
+	@NotNull
+	private String titulo;
+	
+	@Size(min = 4, max = 50) // define o tamanho max, min do caracter do texto
+	private String texto;
+	
+	@UpdateTimestamp // formata a data no pradrão do note
+	private LocalDate data; // pega a data no estilo americano
+
+	// sem os get e set não conseguimos acessar a tabela
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+}
