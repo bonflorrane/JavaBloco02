899e2f91745a296cce9aa02f91c27c7e034a2b1c
bonflorrane <bonfim_lorrane@hotmail.com>
2022-03-25 15:46:17 -0300

    atualizando blog

---
+++ MySQL/DER/AulaAPIblogpessoal.sql
@@ -0,0 +1,5 @@
+SELECT * FROM db_blogpessoal.tb_postagens;
+
+USE db_blogpessoal;
+
+INSERT INTO tb_postagens(data, texto, titulo)  VALUES ('2022-03-24 14:31:30.000', 'Estou aprendendo', 'API Rest Spring');
--- SpringTools/blogpessoal/src/main/java/org/generation/blogpessoal/controller/PostagemController.java
+++ SpringTools/blogpessoal/src/main/java/org/generation/blogpessoal/controller/PostagemController.java
@@ -5,24 +5,57 @@ import java.util.List;
 import org.generation.blogpessoal.model.Postagem;
 import org.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/postagens")
-@CrossOrigin("*")//esta classe aceita requisições de qq origem
+@RestController // permite a utilização dos verbos  e métodos http
+@RequestMapping("/postagens") //onde escreve os endpoints
+@CrossOrigin("*")//esta classe aceita requisições de qq origem(diferentes portas no computador que o front-end pode vir
 public class PostagemController {
 	
-	@Autowired //vai ser acessado a partir do controller
-	private PostagemRepository repository;
+	@Autowired //É a partir desta transferência de responsabilidade que o repository vai ser acessado a partir do controller. Transfere a responsabilidade do controller para o repository
+	private PostagemRepository repository;//deu um apelido para PostagemRepository
 	
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
 		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/titulo/{titulo}") // /titulo/ é para não haver duplicidade com id
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping 
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem)); //no corpo da requisição(@requestbody) mandando texto, titulo. Creado um novo objeto, com nova postagem
+		
+	}
+	
+	@PutMapping 
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
